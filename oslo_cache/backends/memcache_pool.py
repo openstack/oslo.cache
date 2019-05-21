4ff837998421ad6c78e9b9ab1efbecd8d0f95822
Andrii Ostapenko <aostapenko@mirantis.com>
2019-05-21 17:14:31 -0500

    Pass server_max_value_length to memcached client
    Before this commit there was no ability to match client side option
    with the server side one.
    
    Change-Id: Ia76e16b3ed964dde8aa5e104015585fe239ad2b9

--- oslo_cache/backends/memcache_pool.py
+++ oslo_cache/backends/memcache_pool.py
@@ -46,6 +46,8 @@ class PooledMemcachedBackend(memcached_backend.MemcachedBackend):
             arguments={
                 'dead_retry': arguments.get('dead_retry', 5 * 60),
                 'socket_timeout': arguments.get('socket_timeout', 3.0),
+                'server_max_value_length':
+                    arguments.get('server_max_value_length'),
             },
             maxsize=arguments.get('pool_maxsize', 10),
             unused_timeout=arguments.get('pool_unused_timeout', 60),
