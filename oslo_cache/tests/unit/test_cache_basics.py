55cb199f90b27cef203bca8edb9431f9d7ed841c
Hervé Beraud <hberaud@redhat.com>
2022-01-17 14:59:25 +0100

    Expose pymemcache's HashClient public params
    This patch expose a couple of pymemcache's HashClient public
    params that can be useful to configure HA and failover for
    clustered memcached servers.
    
    These options can be used in addition of the previously added
    retrying mechanismes.
    
    This patch rely on recent changes [1] of dogpile.cache that
    aim to expose these options too.
    
    This patch is needed to fix a TLS issue on stable branches introduced by
    pymemcache (since train), where if a cluster node disappear the client
    will fail without retrying to reconnect or to switch to an other node of
    the cluster.
    
    [1] https://gerrit.sqlalchemy.org/c/sqlalchemy/dogpile.cache/+/3528
    
    Partial-Bug: #1959562
    Depends-On: https://review.opendev.org/c/openstack/requirements/+/826995
    Change-Id: I24fc853db4237c08b87871ddff1b3ced27cc7fce
    (cherry picked from commit cb118d04cea318df23831d7445f6b33c866cd0c6)

--- oslo_cache/tests/unit/test_cache_basics.py
+++ oslo_cache/tests/unit/test_cache_basics.py
@@ -514,6 +514,49 @@ class CacheRegionTest(test_cache.BaseTestCase):
             42
         )
 
+    def test_cache_pymemcache_retry_with_extra_opts(self):
+        """Validate we build a valid config for the retry client."""
+        self.config_fixture.config(group='cache',
+                                   enabled=True,
+                                   config_prefix='test_prefix',
+                                   backend='dogpile.cache.pymemcache',
+                                   enable_retry_client=True,
+                                   retry_attempts=42,
+                                   retry_delay=42,
+                                   hashclient_retry_attempts=100,
+                                   hashclient_retry_delay=100,
+                                   dead_timeout=100)
+
+        config_dict = cache._build_cache_config(self.config_fixture.conf)
+
+        self.assertTrue(
+            self.config_fixture.conf.cache.enable_retry_client)
+
+        self.assertEqual(
+            config_dict['test_prefix.arguments.retry_attempts'],
+            42
+        )
+
+        self.assertEqual(
+            config_dict['test_prefix.arguments.retry_delay'],
+            42
+        )
+
+        self.assertEqual(
+            config_dict['test_prefix.arguments.hashclient_retry_attempts'],
+            100
+        )
+
+        self.assertEqual(
+            config_dict['test_prefix.arguments.hashclient_retry_delay'],
+            100
+        )
+
+        self.assertEqual(
+            config_dict['test_prefix.arguments.dead_timeout'],
+            100
+        )
+
     def test_cache_dictionary_config_builder_flush_on_reconnect_enabled(self):
         """Validate we build a sane dogpile.cache dictionary config."""
         self.config_fixture.config(group='cache',
