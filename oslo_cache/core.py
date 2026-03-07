a82e3a5f578df46da7569fca295cc97f037e4281
Takashi Kajinami <kajinamit@oss.nttdata.com>
2026-03-07 20:09:19 +0900

    Fix wrong module name in warning messages
    The correct module name is 'util', not 'utils'.
    
    Change-Id: Ib095fb5426d09dfa251c294360e9fc723c6f7d04
    Signed-off-by: Takashi Kajinami <kajinamit@oss.nttdata.com>

--- oslo_cache/core.py
+++ oslo_cache/core.py
@@ -412,7 +412,7 @@ def function_key_generator(
     to_str: Callable[[Any], str] = str,
 ) -> Callable[..., str]:
     warnings.warn(
-        "Use dogpile.cache.utils.function_key_generator instead",
+        "Use dogpile.cache.util.function_key_generator instead",
         category=DeprecationWarning,
         stacklevel=2,
     )
@@ -425,7 +425,7 @@ def kwarg_function_key_generator(
     to_str: Callable[[Any], str] = str,
 ) -> Callable[..., str]:
     warnings.warn(
-        "Use dogpile.cache.utils.kwarg_function_key_generator instead",
+        "Use dogpile.cache.util.kwarg_function_key_generator instead",
         category=DeprecationWarning,
         stacklevel=2,
     )
